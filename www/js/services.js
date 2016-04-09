96a5cb4738ac550cff2118e4a112af9071c2ffcc
Carlos Roso <ce.roso398@gmail.com>
2016-04-09 02:42:50 -0500

    Added facebook login

--- www/js/services.js
+++ www/js/services.js
@@ -1,5 +1,21 @@
 angular.module('starter.services', ['ngResource'])
 
+.factory('UserService', function() {
+  // For the purpose of this example I will store user data on ionic local storage but you should save it on a database
+  var setUser = function(user_data) {
+    window.localStorage.starter_facebook_user = JSON.stringify(user_data);
+  };
+
+  var getUser = function(){
+    return JSON.parse(window.localStorage.starter_facebook_user || '{}');
+  };
+
+  return {
+    getUser: getUser,
+    setUser: setUser
+  };
+})
+
 .factory('Session',function ($resource){
   return $resource('http://localhost:5000/sessions/:sessionId');
 })
