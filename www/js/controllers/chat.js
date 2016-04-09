26b55e8e0309042f228ae857e5ebb6334b2ba408
Carlos Roso <ce.roso398@gmail.com>
2016-04-09 06:49:30 -0500

    Added chat bubbles. Looking gooooood

--- www/js/controllers/chat.js
+++ www/js/controllers/chat.js
@@ -9,16 +9,29 @@ angular.module('starter.controllers', [])
 // })
 
 .controller('ChatCtrl', function($scope, $rootScope) {
-  console.log("chat ctrl loaded");
-  $scope.messages = [];
-  var message = {
-  	"who":"mibank",
-  	"value":"Hola user"
-  };
-  $scope.messages.push(message);
-  message = {
-  	"who":"user",
-  	"value":"Hola mibank"
-  };
-  $scope.messages.push(message);
+	console.log("chat ctrl loaded");
+	$scope.messages = [];
+	var message = {
+		"who":"mibank",
+		"showavatar":true,
+		"value":"Hola. He sido entrenado durante siglos para este momento. Puedes preguntarme lo que sea, pero ten en cuenta que estoy autorizado a hablar solamente de temas financieros."
+	};
+	$scope.messages.push(message);
+	message = {
+		"who":"user",
+		"showavatar":true,
+		"value":"Hola mibank"
+	};
+	$scope.messages.push(message);
+
+	$scope.newUserMessage = function(){
+		var input = $scope.input.chat;
+		var message = {
+			"who":"user",
+			"showavatar":false,
+			"value":input
+		};
+		$scope.messages.push(message);
+		$scope.input.chat = "";
+	}
 });
